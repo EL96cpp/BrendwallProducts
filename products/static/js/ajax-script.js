021a9ecd330133e13c5161ab93dc671116b03023
EL96cpp <el96cpp@gmail.com>
2024-09-10 18:34:10 +0300

    Included jQuery library

--- products/static/js/ajax-script.js
+++ products/static/js/ajax-script.js
@@ -2,4 +2,57 @@ $(document).ready(function () {
 
     console.log("Hello!");
 
+    $(document).on("click", "#add_product_btn", function (e) {
+
+        console.log("Click add!");
+
+        e.preventDefault();
+
+        var remove_url = $(this).attr("href");
+
+        console.log(remove_url);
+
+        // $.ajax({
+            
+        //     type: "POST",
+        //     url: remove_url,
+        //     data: {
+        //         cart_id: cart_id,
+        //         csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
+        //     },
+
+        //     success: function (data) {
+
+        //         var cartItemsContainer = $("#cart-items-container");
+        //         cartItemsContainer.html(data.cart_items_html);
+
+        //         cart_count -= data.quantity_deleted;
+        //         goods_in_cart_count.text(cart_count);
+
+
+        //         var cart_items_container = $("#cart-items-container");
+        //         cart_items_container.html(data.cart_items_html);
+
+        //         var orderConfirmationWrapper = $("#order_confirmation_wrapper");
+        //         orderConfirmationWrapper.html(data.cart_items_html);
+                
+
+        //         if (data.redirect) {
+
+        //             window.location = "/";
+
+        //         }
+
+        //     },
+
+        //     error: function (data) {
+        //         console.log("Ошибка при удалении корзины");
+        //     }
+            
+        // });
+
+    });
+
+
+
 });
