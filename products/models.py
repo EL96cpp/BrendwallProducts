878882f8b5650f91d2c46d1217d99e2be6253aba
EL96cpp <el96cpp@gmail.com>
2024-09-10 14:40:50 +0300

    Added Product model, registered it in django-admin

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False, verbose_name="Название")
+    description = models.TextField(null=False, blank=False, verbose_name="Описание")
+    price = models.PositiveIntegerField(null=False, blank=False, verbose_name="Цена")
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
