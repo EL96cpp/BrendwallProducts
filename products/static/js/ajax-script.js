624088320130f5f57dc0ea95b2bd7bb07a6385d5
EL96cpp <el96cpp@gmail.com>
2024-09-11 12:59:11 +0300

    Added fade out messages to ajax script when product added or error occured

--- products/static/js/ajax-script.js
+++ products/static/js/ajax-script.js
@@ -1,18 +1,12 @@
 $(document).ready(function () {
 
-    console.log("Hello!");
-
     const form = document.getElementById('add_product_form')
 
     $(document).on("click", "#add_product_btn", function (e) {
 
-        console.log("Click add!");
-
         const product_name = document.getElementById('id_name');
         const product_description = document.getElementById('id_description');
         const product_price = document.getElementById('id_price');
-        
-        console.log(product_name.value, product_description.value, product_price.value);
 
         e.preventDefault();
 
@@ -33,16 +27,27 @@ $(document).ready(function () {
 
                 console.log("New product added!");
 
-                if (data.redirect) {
+                var productList = $("#products_list");
+                productList.html(data.products_html);
 
-                    window.location = "/";
+                var message_span = $("#message_span");
+                message_span.text(data.message);
+                var message_wrapper = $("#message_wrapper");
+                message_wrapper.show();
+                message_wrapper.delay(2000).fadeOut();
 
-                }
 
             },
 
             error: function (data) {
-                console.log("Error");
+
+                console.log(data.responseJSON.message);
+                var message_span = $("#message_span");
+                message_span.text(data.responseJSON.message);
+                var message_wrapper = $("#message_wrapper");
+                message_wrapper.show();
+                message_wrapper.delay(2000).fadeOut();
+
             }
             
         });
--- products/views.py
+++ products/views.py
@@ -1,27 +1,27 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.template.loader import render_to_string
 
 from .models import Product
 from .forms import AddProductForm
 
 
 def product_add(request):
-    print("product add")
     product_name = request.POST.get("product_name")
     product_description = request.POST.get("product_description")
     product_price = request.POST.get("product_price")
+    print(product_name, product_description, product_price)
     if not product_name or not product_description or not product_price:
-        return JsonResponse({'message': 'Все поля должны быть заполнены!'}, status=400)
+        return JsonResponse({'message': 'Заполните все необходимые поля!'}, status=400)
     if not product_price.isdigit():
         return JsonResponse({'message': 'Цена должна быть целым числом!'}, status=400)
     if int(product_price) <= 0:
-        return JsonResponse({'message': 'Цена должна быть положительным целым числом!'}, status=400)
+        return JsonResponse({'message': 'Цена должна быть целым числом больше 0!'}, status=400)
     Product.objects.create(name=product_name, description=product_description, price=int(product_price))
-    return JsonResponse({'product_name': product_name, 
-                         'product_description': product_description,
-                         'product_price': product_price}, 
-                         status=200)
-
+    products = Product.objects.all()
+    products_html = render_to_string("includes/products.html", {"products": products}, request=request)
+    return JsonResponse({'message': 'Товар успешно добавлен!', 
+                         'products_html': products_html}, status=200)
 
 
 def products_list(request):
